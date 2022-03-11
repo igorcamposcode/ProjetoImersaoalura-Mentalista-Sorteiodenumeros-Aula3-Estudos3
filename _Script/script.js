fc9df9191b9091e4d43a1d68b9fc65b9da047849
Igor Campos <Igorcampos_45@yahoo.com>
2022-03-11 00:26:46 -0300

    Atualização de variaveis - imersão dev alura - aula 3

--- _Script/script.js
+++ _Script/script.js
@@ -18,50 +18,50 @@ var numeroSecreto = parseInt(Math.random() * 21 );
      }
  }
 
-var numeroSecreto = parseInt(Math.random() * 41);
+var numeroSecreto2 = parseInt(Math.random() * 41);
 
  function Chutar2() {
     var elementoResultado = document.getElementById("resultado2");
       var chute2 = parseInt(document.getElementById("valor2").value);
       console.log(chute2);
 
-      if (chute2 == numeroSecreto){
+      if (chute2 == numeroSecreto2){
           elementoResultado.innerHTML = "Parabéns, você acertou... ";
       }else if (chute2 >40 || chute2 < 0){
           elementoResultado.innerHTML = "Vocês devem digitar um (2) número de 0 a 40";
       }else {
-          elementoResultado.innerHTML = "Triste que você errou, O Número Secreto era? " + numeroSecreto;
+          elementoResultado.innerHTML = "Triste que você errou, O Número Secreto era? " + numeroSecreto2;
       }
  }
 
- var numeroSecreto = parseInt(Math.random() * 71);
+ var numeroSecreto3 = parseInt(Math.random() * 71);
 
   function Chutar3() {
       var elementoResultado = document.getElementById("resultado3");
         var chute3 = parseInt(document.getElementById("valor3").value);
         console.log(chute3);
 
-        if (chute3 == numeroSecreto) {
+        if (chute3 == numeroSecreto3) {
             elementoResultado.innerHTML = "Parabéns, você acertou... ";
         }else if (chute3 > 70 || chute3 < 0){
             elementoResultado.innerHTML = "Vocês devem digitar um (2) número de 0 a 70";
         }else {
-            elementoResultado.innerHTML = "Triste que você errou, O Número Secreto era? " + numeroSecreto;
+            elementoResultado.innerHTML = "Triste que você errou, O Número Secreto era? " + numeroSecreto3;
         }
   }
 
-  var numeroSecreto = parseInt(Math.random() * 101);
+  var numeroSecreto4 = parseInt(Math.random() * 101);
 
   function Chutar4() {
       var elementoResultado = document.getElementById("resultado4");
         var chute4 = parseInt(document.getElementById("valor4").value);
         console.log(chute4);
 
-        if (chute4 == numeroSecreto) {
+        if (chute4 == numeroSecreto4) {
             elementoResultado.innerHTML = "Parabéns, você acertou... ";
         }else if (chute4 > 100 || chute4 < 0){
             elementoResultado.innerHTML = "Vocês devem digitar um (2) número de 0 a 70";
         }else {
-            elementoResultado.innerHTML = "Triste que você errou, O Número Secreto era? " + numeroSecreto;
+            elementoResultado.innerHTML = "Triste que você errou, O Número Secreto era? " + numeroSecreto4;
         }
   }
